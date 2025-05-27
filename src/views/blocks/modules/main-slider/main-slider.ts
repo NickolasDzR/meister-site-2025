d3fc873d68b558c0011947162e3b92b3afe9222c
NickolasDzR <nikolyae2019@gmail.com>
2025-05-27 13:10:51 +0300

    Слайдер на главной

--- src/views/blocks/modules/main-slider/main-slider.ts
+++ src/views/blocks/modules/main-slider/main-slider.ts
@@ -8,19 +8,38 @@ if (mainSlider) {
 
     if (slides.length > 0) {
         const mainSliderSplide = mainSlider.querySelector(".main-slider__slider") as HTMLDivElement;
+        const slideCounter = mainSlider.querySelector(".main-slider__slide-count") as HTMLDivElement;
+        const sliderLength = mainSlider.querySelectorAll(".main-slider__slide:not(.splide__slide--clone)") as NodeListOf<HTMLLIElement>
 
         const sliderSettings = {
             type: 'loop',
-            // autoplay: true,
-            arrows: true,
+            autoplay: true,
             speed: 600,
 
         } as Options;
 
         if (mainSliderSplide) {
-            const splideSlider = new Splide(mainSliderSplide, sliderSettings)
+            const splideSlider = new Splide(mainSliderSplide, sliderSettings);
+
+            if (slideCounter) {
+                splideSlider.on("move", (newIndex) => {
+                    slideCounter.textContent = `${ newIndex + 1 } / ${ sliderLength.length }`;
+                });
+            }
+
+            const progressCircle = mainSlider.querySelector(".main-slider__progress-bar") as SVGAElement;
+
+            if (progressCircle) {
+                const dashoffset = 314;
+
+                splideSlider.on("autoplay:playing", (rate) => {
+                    console.log(`-${rate * 314}`)
+                    progressCircle.style.strokeDashoffset = `-${rate * dashoffset}`;
+                })
+            }
 
             splideSlider.mount();
+
         }
     }
 }
